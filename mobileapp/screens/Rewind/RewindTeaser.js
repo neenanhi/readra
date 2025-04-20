13631593601ba454a896f5893fbb6b1a41fc0792
Neena Nguyen <neenanhi@gmail.com>
2025-04-20 04:35:40 -0700

    rewind teaser

---
+++ mobileapp/screens/Rewind/RewindTeaser.js
@@ -0,0 +1,5 @@
+//RewindTeaser.js
+// Teaser / Splash Screen
+// 	• As soon as the user taps “Rewind,” show a very brief fullscreen animation or graphic—maybe a vintage book gently opening (faded‐in GLSL shader + overlaid text: “Your Monthly Reading Rewind).
+// 	• Have it auto‐fade out after 1–1.5 seconds (or let the user tap to skip), so it feels like a teaser, not a full step.
+// RewindTeaser.js
