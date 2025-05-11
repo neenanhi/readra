6aafea8c90229448c6e079d33af893ed5d0be5b7
Neena Nguyen <neenanhi@gmail.com>
2025-05-11 10:57:23 -0700

    Rewind1 ripple animation

--- mobileapp/screens/Rewind/Rewind1.js
+++ mobileapp/screens/Rewind/Rewind1.js
@@ -205,4 +205,14 @@ export default function Rewind1({ onNext }) {
     };
   }, []);
 
- 
+  function onContextCreate(gl) {
+    // Compile vertex shader
+    const vertexShaderSource = `
+      attribute vec2 a_position;
+      varying vec2 v_uv;
+      void main() {
+        v_uv = a_position * 0.5 + 0.5;
+        gl_Position = vec4(a_position, 0, 1);
+      }
+    `;
+    
