3f32293f9be97a7965d9dcb45d7ef5678afe309e
Neena Nguyen <neenanhi@gmail.com>
2025-05-18 10:59:47 -0700

    Rewind1 ripple animation

--- mobileapp/screens/Rewind/Rewind1.js
+++ mobileapp/screens/Rewind/Rewind1.js
@@ -263,3 +263,11 @@ export default function Rewind1({ onNext }) {
     const buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, quadVerts, gl.STATIC_DRAW);
+
+    const posLoc = gl.getAttribLocation(program, "a_position");
+    gl.enableVertexAttribArray(posLoc);
+    gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 0, 0);
+
+    // Initialize timing
+    start.current = performance.now();
+    const DURATION = 3000; // run animation for 3 seconds
